723e5f8fb04ed460a8520a24543c406cbf93f34b
Steven Schubiger <schubiger@cpan.org>
2020-10-03 22:50:45 +0200

    released 1.11_01

--- lib/DateTime/Format/Natural.pm
+++ lib/DateTime/Format/Natural.pm
@@ -21,7 +21,7 @@ use Params::Validate ':all';
 use Scalar::Util qw(blessed);
 use Storable qw(dclone);
 
-our $VERSION = '1.11';
+our $VERSION = '1.11_01';
 
 validation_options(
     on_fail => sub
